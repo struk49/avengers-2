1278872bb23b41fbbc96a1a495330ef636bb63f7
struk49 <andrewstruk@yahoo.co.uk>
2024-02-28 12:22:52 +0000

    trying to connect mongodb

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from flask import (
     Flask, flash, render_template,
-     redirect, request, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
